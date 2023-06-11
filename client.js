7e71493d51a75bb422ab21d4a6f4872e5ea2b577
Baby <89675173+Swag666baby@users.noreply.github.com>
2023-06-11 00:59:48 -0300

    Update client.js
    switching to english

--- client.js
+++ client.js
@@ -8,11 +8,11 @@ const input = readline.createInterface({
 	output: process.stdout
 })
 let nickName = ''
-input.question('Digite o seu nickname: ', name => {
+input.question('enter your nickname : ', name => {
     nickName = name
     client.connect(4000, "127.0.0.1", () => {
         exec("clear")
-        console.log(nickName, "conectado!")
+        console.log(nickName, "connected !")
         input.addListener('line', line => client.write(`${nickName}: ${line}`))
         client.on('data', data => console.log(data.toString()))
     })
