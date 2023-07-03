69aa56eb959bf9a84128a148650e053a239874e8
Baby <89675173+Swag666baby@users.noreply.github.com>
2023-07-03 12:17:08 -0300

    Update client.js

--- client.js
+++ client.js
@@ -1,6 +1,5 @@
 const net = require('net')
 const readline = require('readline')
-const {exec} = require("child_process")
 const client = new net.Socket()
 
 const input = readline.createInterface({
